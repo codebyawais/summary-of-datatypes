403f95291a1fbfb3ae4e3713c2e1ef71d7cabd90
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-05 08:37:23 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+// primitive
+// String
+const str ="awais";
+console.log(str);
+// num/
+const num = 123;
+console.log(num);
+// Boolean
+const isloggedin = true;
+console.log(isloggedin);
+// null
+const temp = null;
+console.log(temp);
+// undefined.
+let email;
+console.log(email);
+// Symbol
+const id =Symbol('1234');
+const newid = Symbol('1234')
+console.log(id === newid);
+// bigint
+const bigint =1234562345234n
+console.log(bigint);
+
+
+
+
+// non premitive 
+// Array
+const hero = [1,2,3,4,5];
+
+
+// Object
+let myobj{
+    name="awais",
+    age=23,
+
+}
+
+const myfunction= function(){
+    console.log("hello world");
+}
+
+
+
+
+
+
